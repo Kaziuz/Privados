86ddf5c36c93e0b2837b0e2ac4e794a4bddec5b8
kaziuz <johnnysepulveda988@gmail.com>
2022-10-30 14:02:36 -0500

    advance ui

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <!-- <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-    <p class="text-3xl text-gray-700 font-bold mb-5">
-      Welcome!
-    </p>
-    <p class="text-gray-500 text-lg">
-      Vue and Tailwind CSS in action
-    </p>
- </div> -->
-  <div class="relative d-block">
-    <div class="border border-black w-full h-screen bg-red-400 absolute px-1 py-1">
-      <div class="bg-blue-200 w-full h-full d-flex opacity-1">
-        <pre>{{ currentData }}</pre>
+  <div class="relative d-block font-sans">
+    <div class="w-full h-screen absolute px-1 py-1 bg-black text-white">
+      <!-- en el eje x jusitfy-start  justify-center justify-end -->
+      <!-- en el eje x items-start items-center items-start items-ens-->
+      <div class="w-full h-1/2 flex justify-center items-center">
+        <span class="text-4xl">
+          {{ currentData.question }}
+        </span>
+      </div>
+      <div class="w-full h-1/2 flex flex-col justify-end items-center">
+        <span class="text-2xl italic">
+          {{ currentData.answer }}
+        </span>
+        <div class="flex justify-end items-end">
+          <span class="text-xl">
+            {{ currentData.nickname }}
+          </span>
+          <span class="text-sm ml-2" style="line-height:24px">{{ currentData.age }}</span>
+        </div>
       </div>
       <button
         @click="startProgram = true"
@@ -39,33 +46,44 @@ export default {
     let stepperProgram = () => {}
     let frame = () => {}
     const privateData = reactive([
-      { id: 0,question:'Pregunta 1', answer: 'Respuesta 1', nickname: 'anonimo 1', age: 19 },
-      { id: 1,question:'Pregunta 2', answer: 'Respuesta 2', nickname: 'anonimo 2', age: 19 },
-      { id: 2,question:'Pregunta 3', answer: 'Respuesta 3', nickname: 'anonimo 3', age: 19 },
-      { id: 3,question:'Pregunta 4', answer: 'Respuesta 4', nickname: 'anonimo 4', age: 19 },
-      { id: 4,question:'Pregunta 5', answer: 'Respuesta 5', nickname: 'anonimo 5', age: 19 },
-      { id: 5,question:'Pregunta 6', answer: 'Respuesta 6', nickname: 'anonimo 6', age: 19 },
-      { id: 6,question:'Pregunta 7', answer: 'Respuesta 7', nickname: 'anonimo 7', age: 19 },
-      { id: 7,question:'Pregunta 8', answer: 'Respuesta 8', nickname: 'anonimo 8', age: 19 },
-      { id: 8,question:'Pregunta 9', answer: 'Respuesta 9', nickname: 'anonimo 9', age: 19 },
-      { id: 9,question:'Pregunta 10', answer: 'Respuesta 1', nickname: 'anonimo 1', age: 19 },
-      { id: 10,question:'Pregunta 11', answer: 'Respuesta 2', nickname: 'anonimo 2', age: 19 },
-      { id: 11,question:'Pregunta 12', answer: 'Respuesta 3', nickname: 'anonimo 3', age: 19 },
-      { id: 12,question:'Pregunta 13', answer: 'Respuesta 4', nickname: 'anonimo 4', age: 19 },
-      { id: 13,question:'Pregunta 14', answer: 'Respuesta 5', nickname: 'anonimo 5', age: 19 },
-      { id: 14,question:'Pregunta 15', answer: 'Respuesta 6', nickname: 'anonimo 6', age: 19 },
-      { id: 15,question:'Pregunta 16', answer: 'Respuesta 7', nickname: 'anonimo 7', age: 19 },
-      { id: 16,question:'Pregunta 17', answer: 'Respuesta 8', nickname: 'anonimo 8', age: 19 },
-      { id: 17,question:'Pregunta 18', answer: 'Respuesta 9', nickname: 'anonimo 9', age: 19 },
-      { id: 18,question:'Pregunta 19', answer: 'Respuesta 1', nickname: 'anonimo 1', age: 19 },
-      { id: 19,question:'Pregunta 20', answer: 'Respuesta 2', nickname: 'anonimo 2', age: 19 },
-      { id: 20,question:'Pregunta 21', answer: 'Respuesta 3', nickname: 'anonimo 3', age: 19 },
-      { id: 21,question:'Pregunta 22', answer: 'Respuesta 4', nickname: 'anonimo 4', age: 19 },
-      { id: 22,question:'Pregunta 23', answer: 'Respuesta 5', nickname: 'anonimo 5', age: 19 },
-      { id: 23,question:'Pregunta 24', answer: 'Respuesta 6', nickname: 'anonimo 6', age: 19 },
-      { id: 24,question:'Pregunta 25', answer: 'Respuesta 7', nickname: 'anonimo 7', age: 19 },
-      { id: 25,question:'Pregunta 26', answer: 'Respuesta 8', nickname: 'anonimo 8', age: 19 },
-      { id: 26,question:'Pregunta 27', answer: 'Respuesta 9', nickname: 'anonimo 9', age: 19 },
+      { id: 0,
+        question:'¿De qué se alimentan los koalas?',
+        answer: 'Los koalas son animales herbívoros que tienen una dieta poco variada. Se alimentan de hojas de eucalipto.',
+        nickname: 'anonimo 1',
+        age: '23 años'
+      },
+      {
+        id: 1,
+        question:'¿De qué se alimentan los koalas?',
+        answer: 'Los koalas son animales herbívoros que tienen una dieta poco variada. Se alimentan de hojas de eucalipto.',
+        nickname: 'anonimo 2',
+        age: '23 años'
+      },
+      { id: 2,question:'Pregunta 3', answer: 'Respuesta 3', nickname: 'anonimo 3', age: '23 años' },
+      { id: 3,question:'Pregunta 4', answer: 'Respuesta 4', nickname: 'anonimo 4', age: '23 años' },
+      { id: 4,question:'Pregunta 5', answer: 'Respuesta 5', nickname: 'anonimo 5', age: '23 años' },
+      { id: 5,question:'Pregunta 6', answer: 'Respuesta 6', nickname: 'anonimo 6', age: '23 años' },
+      { id: 6,question:'Pregunta 7', answer: 'Respuesta 7', nickname: 'anonimo 7', age: '23 años' },
+      { id: 7,question:'Pregunta 8', answer: 'Respuesta 8', nickname: 'anonimo 8', age: '23 años' },
+      { id: 8,question:'Pregunta 9', answer: 'Respuesta 9', nickname: 'anonimo 9', age: '23 años' },
+      { id: 9,question:'Pregunta 10', answer: 'Respuesta 1', nickname: 'anonimo 1', age: '23 años' },
+      { id: 10,question:'Pregunta 11', answer: 'Respuesta 2', nickname: 'anonimo 2', age: '23 años' },
+      { id: 11,question:'Pregunta 12', answer: 'Respuesta 3', nickname: 'anonimo 3', age: '23 años' },
+      { id: 12,question:'Pregunta 13', answer: 'Respuesta 4', nickname: 'anonimo 4', age: '23 años' },
+      { id: 13,question:'Pregunta 14', answer: 'Respuesta 5', nickname: 'anonimo 5', age: '23 años' },
+      { id: 14,question:'Pregunta 15', answer: 'Respuesta 6', nickname: 'anonimo 6', age: '23 años' },
+      { id: 15,question:'Pregunta 16', answer: 'Respuesta 7', nickname: 'anonimo 7', age: '23 años' },
+      { id: 16,question:'Pregunta 17', answer: 'Respuesta 8', nickname: 'anonimo 8', age: '23 años' },
+      { id: 17,question:'Pregunta 18', answer: 'Respuesta 9', nickname: 'anonimo 9', age: '23 años' },
+      { id: 18,question:'Pregunta 19', answer: 'Respuesta 1', nickname: 'anonimo 1', age: '23 años' },
+      { id: 19,question:'Pregunta 20', answer: 'Respuesta 2', nickname: 'anonimo 2', age: '23 años' },
+      { id: 20,question:'Pregunta 21', answer: 'Respuesta 3', nickname: 'anonimo 3', age: '23 años' },
+      { id: 21,question:'Pregunta 22', answer: 'Respuesta 4', nickname: 'anonimo 4', age: '23 años' },
+      { id: 22,question:'Pregunta 23', answer: 'Respuesta 5', nickname: 'anonimo 5', age: '23 años' },
+      { id: 23,question:'Pregunta 24', answer: 'Respuesta 6', nickname: 'anonimo 6', age: '23 años' },
+      { id: 24,question:'Pregunta 25', answer: 'Respuesta 7', nickname: 'anonimo 7', age: '23 años' },
+      { id: 25,question:'Pregunta 26', answer: 'Respuesta 8', nickname: 'anonimo 8', age: '23 años' },
+      { id: 26,question:'Pregunta 27', answer: 'Respuesta 9', nickname: 'anonimo 9', age: '23 años' },
     ])
 
     watchEffect(() => {
