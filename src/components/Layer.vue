25c9ccd092ed27a734121d7f3dd6e7d97840c633
brolin <brolin108@gmail.com>
2022-11-14 22:55:16 -0500

    Add custom extra styles

--- src/components/Layer.vue
+++ src/components/Layer.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="absolute text-white">
     <div class="relative flex flex-col justify-around w-screen h-screen p-8">
-            <div class="text-4xl font-black">
+            <div 
+            :class="`${extraCss}`"
+            class="text-4xl font-black">
               {{qa_obj?.question}}
             </div>              
             <div class="text-2xl italic p-4 font-thin text-right">
@@ -13,6 +15,7 @@
 
 <script setup>
     defineProps({
+        extraCss: String,
         nOpacity: Number,
         qa_obj: Object,
         milliseconds: Number
